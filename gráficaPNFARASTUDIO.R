d7d86915624c926a7e89275581cfa1a190b8b583
Ainoafv <73110287+Ainoafv@users.noreply.github.com>
2020-10-21 19:01:16 +0200

    Add files via upload

--- gráficaPNFARASTUDIO.R
+++ gráficaPNFARASTUDIO.R
@@ -3,7 +3,7 @@ library("ggplot2")
 
 p2mtb2
 
-ggplot(data=p2mtb2, aes(x= PNF, y=Abs, colour=Abs ))+ geom_smooth(method = "lm", se=FALSE, color="red", formula = y ~x ) + geom_point(size=3)
+ggplot(data=p2mtb2, aes(x= PNF, y=Abs, colour=Abs ))+ geom_smooth(method = "lm", se=FALSE, color="red", formula = y ~ x ) + geom_point(size=3)
                                                                                                                     
 
 
